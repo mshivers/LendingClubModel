418a1d5a61fdb0685845f27492884d894c6b8ba6
Marc Shivers <marc.shivers@gmail.com>
2018-05-06 18:08:27 -0400

    add updated cron.py file

--- cron.py
+++ cron.py
@@ -5,7 +5,7 @@ import invest
 def main():
     model_dir = constants.PathManager.get_dir('training') 
     pm = invest.PortfolioManager(model_dir=model_dir, new_only=True)
-    pm.try_for_awhile(3)
+    pm.try_for_awhile(4)
 
 if __name__=='__main__':
     main()
